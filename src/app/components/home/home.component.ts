f1315357e3a2676aa9f1273011e96f8aa0a2e0b5
KarlaFelix1 <e-kmfelix@neoris.com>
2022-01-12 11:26:22 -0700

    home con problemas de tamaño

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  boxes: any;
+  constructor() {
+    this.boxes=[{
+      nombre:'Venta',
+      icono:'iconoDolar fa fa-dollar-sign fa-2x icono-id',
+      moneyP4: '$ 1.00.000',
+      moneyP11:'$ 2.000.000',
+      moneyVar:'$ 1.000.000'
+    },
+    {
+      nombre:'Unidades',
+      icono:'iconoCaja fas fa-box-open fa-2x',
+      moneyP4: '$ 300.000',
+      moneyP11:'$ 1.000.000',
+      moneyVar:'$ 700.000'
+    }
+  ]
   }
 
+  ngOnInit(): void {}
 }
