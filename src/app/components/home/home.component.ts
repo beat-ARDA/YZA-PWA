aff56cd4297a4549f62f90d87e542e0481ecb7a2
KarlaFelix1 <e-kmfelix@neoris.com>
2022-01-12 20:19:32 -0700

    cambio de box a card para arreglar el espacio entre navbar y cards

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,23 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  boxes: any;
+  cards: any;
   constructor() {
-    this.boxes=[{
-      nombre:'Venta',
-      icono:'iconoDolar fa fa-dollar-sign fa-2x icono-id',
-      moneyP4: '$ 1.00.000',
-      moneyP11:'$ 2.000.000',
-      moneyVar:'$ 1.000.000'
-    },
-    {
-      nombre:'Unidades',
-      icono:'iconoCaja fas fa-box-open fa-2x',
-      moneyP4: '$ 300.000',
-      moneyP11:'$ 1.000.000',
-      moneyVar:'$ 700.000'
-    }
-  ]
+    this.cards = [
+      {
+        nombre: 'Venta',
+        icono: 'iconoDolar fa fa-dollar-sign fa-2x icono-id',
+        moneyP4: '$ 1.00.000',
+        moneyP11: '$ 2.000.000',
+        moneyVar: '$ 1.000.000',
+      },
+      {
+        nombre: 'Unidades',
+        icono: 'iconoCaja fa fa-box-open fa-2x',
+        moneyP4: '$ 300.000',
+        moneyP11: '$ 1.000.000',
+        moneyVar: '$ 700.000',
+      },
+      {
+        nombre: 'Contribución',
+        icono: 'iconoDolar fa fa-dollar-sign fa-2x icono-id',
+        moneyP4: '$ 400.000',
+        moneyP11: '$ 1.200.000',
+        moneyVar: '$ 800.000',
+      },
+      {
+        nombre: 'Margen',
+        icono: 'iconoDolar fa fa-dollar-sign fa-2x icono-id',
+        moneyP4: '30%',
+        moneyP11: '60%',
+        moneyVar: '30%',
+      },
+    ];
   }
 
   ngOnInit(): void {}
