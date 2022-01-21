e1fbf33cdf098730e6ae66350a4cb330474da39e
KarlaFelix1 <e-kmfelix@neoris.com>
2022-01-21 16:32:11 -0700

    segundo navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,6 @@ export class AppComponent {
 
   ngDoCheck() {
     this.url = this.router.url;
+    
   }
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -14,11 +14,17 @@ export class MenuComponent implements OnInit {
   public ultimoElemento: string;
   public url: string;
 
+  ngDoCheck() {
+    this.url = this._router.url;
+    console.log(this.url, 'hola');
+    
+  }
   constructor(private _router: Router) {
     this.url ="/home";
     this._router.events.subscribe((val) => {
       console.log(val);
     })
+    
     this.items = [
       new ItemMenu("SOI", false, "SOI", "../../../assets/Android/hdpi/ic_soi_on.png", "../../../assets/Android/hdpi/ic_soi_off.png"),
       new ItemMenu("RESUMEN BEBE", false, "Bebe", "../../../assets/Android/hdpi/ic_bb_on.png", "../../../assets/Android/hdpi/ic_bb_off.png"),
@@ -27,12 +33,12 @@ export class MenuComponent implements OnInit {
       new ItemMenu("RESUMEN EVO FARMA", false, "EVO Farma", "../../../assets/Android/hdpi/ic_evo_farma_on.png", "../../../assets/Android/hdpi/ic_evo_farma_off.png")
     ];
     this.itemsDetalles = [
-      new ItemMenu("RESUMEN FARMA", false, "Farma", "../../../assets/Android/hdpi/ic_farma_on.png", "../../../assets/Android/hdpi/ic_farma_off.png"),
-      new ItemMenu("RESUMEN FARMA AC", false, "Farma AC", "../../../assets/Android/hdpi/ic_farma_ac_on.png", "../../../assets/Android/hdpi/ic_farma_ac_off.png"),
-      new ItemMenu("RESUMEN EVO FARMA", false, "EVO Farma", "../../../assets/Android/hdpi/ic_evo_farma_on.png", "../../../assets/Android/hdpi/ic_evo_farma_off.png")
+      new ItemMenu("COMPORTAMIENTO", false, "Comportamiento", "../../../assets/Android/hdpi/ic_comportamiento_on.png", "../../../assets/Android/hdpi/ic_comportamiento_off.png"),
+      new ItemMenu("PORCENTAJES", false, "Porcentajes", "../../../assets/Android/hdpi/ic_porcentajes_on.png", "../../../assets/Android/hdpi/ic_porcentajes_off.png"),
+      new ItemMenu("VARIACION", false, "Variación $$-%", "../../../assets/Android/hdpi/ic_variacion_on.png", "../../../assets/Android/hdpi/ic_variacion_off.png")
     ];
-    this.items[0].activado = true;
-    this.ultimoElemento = this.items[0].nombre;
+    this.items[0].activado  = true;
+    this.ultimoElemento = this.url=== '/detalles'?this.itemsDetalles[0].nombre: this.items[0].nombre;
   }
 
   ngOnInit(): void {
@@ -54,6 +60,8 @@ export class MenuComponent implements OnInit {
       this.ultimoElemento = _item;
     }
   }
+
+  
 }
 
 
