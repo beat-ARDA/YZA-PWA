5029b16c0c72acc56a42afcb757da4a094152774
KarlaFelix1 <e-kmfelix@neoris.com>
2022-01-20 19:32:07 -0700

    componente cuerpo-busqueda sin header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { HeaderService } from 'src/app/services/header.service';
 
 @Component({
@@ -8,10 +9,36 @@ import { HeaderService } from 'src/app/services/header.service';
 })
 export class HeaderComponent implements OnInit {
   message!: string;
-  
-constructor(private helper: HeaderService) { }
+  detalleVal:boolean = true;
+  currentUrl!: string;
+constructor(private router:Router, private helper: HeaderService) {
+  this.router.events.subscribe((event) => {
+
+    if (event instanceof NavigationEnd) {
+
+      this.currentUrl = event.url
+
+     
+      if(this.currentUrl === '/detalles'){
+        this.detalleVal = false;
+        console.log('NavigationEnd:', event.url);
+      }
+      else{
+        this.detalleVal=true;
+      }
+    }
+
+  })
+ 
+}
+
 ngOnInit() {
-    this.helper.customMessage.subscribe(msg => this.message = msg);
+   this.helper.customMessage.subscribe(msg => this.message = msg);
+   if(this.router.url === '/detalles'){
+     console.log('hola');
+   }
+   console.log(this.router.url);
+   
   }
 
 }
