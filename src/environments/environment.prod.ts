f44a4cda13c375322349d08095eaece3a1e58f01
Ricardo Cardeña Sanchez <ricardo.cardena@neoris.com>
2022-02-03 12:27:37 -0600

    adding prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'http://20.96.44.96:9090',
+  ENDPOINT_PERIOD: '/api/Periodo',
+  ENDPOINT_SOIRESUME: '/api/ResumenGeneral',
+  ENDPOINT_CATEGORIES: '/api/Categorias',
+  ENDPOINT_ITEMS: '/api/Articulos'
 };
